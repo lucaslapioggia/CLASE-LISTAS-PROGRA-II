e964151d465931f4a4b7e366ff4ce81a59605762
lucaslapioggia <73617414+lucaslapioggia@users.noreply.github.com>
2022-09-08 10:27:11 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "libreria.h"
+#include "libreria.c"
+
+void mostrarListaL(nodo* lista);
+nodoL* agregarAlFinal(nodoL*lista, nodoL* nuevo);
+nodoL*buscarUltimoL(nodoL*lista);
+
+typedef struct
+{
+    char nombre[30];
+    int edad;
+}Persona;
+
+typedef struct nodoL
+{
+    Persona dato;
+    struct nodoL* siguiente;
+}
+
+int main()
+{
+
+    return 0;
+}
+/*
+void mostrarPersonaL(Persona dato)
+{
+    printf("-----------------\n");
+    printf()
+}*/
+void mostrarListaL(nodo* lista)
+{
+    nodo* seguidora= lista; //ver si vacia p mensaje
+
+    while(seguidora!=NULL)
+    {
+        mostrarPersonaL(seguidora->dato); //o mod: mostrar nodo
+        seguidora= seguidora -> siguiente;
+    }
+}
+
+nodoL* agregarAlFinal(nodoL*lista, nodoL* nuevo)
+{
+
+    nodoL* ultimo = buscarUltimoL(lista);
+    ultimo ->siguiente = nuevo;
+
+    return lista;
+
+}
+
+nodoL*buscarUltimoL(nodoL*lista)
+{
+    nodoL* ultimo= lista;
+
+    while(ultimo->siguiente !=NULL)
+    {
+        ultimo = ultimo ->siguiente;
+    }
+
+
+    return ultimo;
+}
+
+
+nodoL*buscarUltimoL(nodoL*lista)
+{
+    if(lista==  NULL)
+    {
+        lista=nuevo;
+    }
+
+    else
+    {
+        nodo*ultimo = buscarUltimoL(lista);
+        ultimo->siguiente = nuevo;
+    }
+    return lista;
+}
+
+nodoL* buscarDato(nodoL* lista, char nombreBusc[])
+{
+    nodoL* seguidora = lista;
+
+    while(seguidora!=NULL && strcmpi(seguidora ->dato.nombre, nombreBusc)!=0)
+    {
+        seguidora = seguidora ->siguiente;
+    }
+
+    return seguidora;
+}
+
+insertarEnOrden()
